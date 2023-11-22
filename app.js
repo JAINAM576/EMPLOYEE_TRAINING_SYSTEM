d9cfb03bdf9047a3514cea681ade6397bcf4ea94
unknown <parmarparth954@gmail.com>
2023-11-23 02:52:52 +0530

    all updates

--- app.js
+++ app.js
@@ -18,11 +18,10 @@ require('dotenv').config();
 
 
 var pool = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port:process.env.port,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+  host: "localhost",
+  database: "ps008001-db",
+  user: "root",
+  password: "root",
   dateStrings: true
 });
 
@@ -1637,24 +1636,10 @@ app.post('/adding_marks', (req, res) => {
           mark,
           100
         ], (error, result) => {
-
-          if (error) {
-            console.log(error, "error")
-            res.status(200).json(0)
-          }
-          else {
-            res.status(200).json(1)
-
-          }
-
         })
-
       })
     } else {
-
-
-      res.status(500).json(0)
-
+      res.status(500).json(0);
     }
   })
 })
@@ -1699,7 +1684,7 @@ app.post("/spipa-training-req-subject", (req, res) => {
 
 
 //download excel from table
-app.get('/excel/emp_exam', (req, res) => {
+app.get('/excel/emp_exam2', (req, res) => {
   pool.query('SELECT * FROM  emp_exam_result', (error, results) => {
     if (error) throw error;
     var data = [];
@@ -1724,6 +1709,30 @@ app.get('/excel/emp_exam', (req, res) => {
   });
 });
 
+
+app.get("/excel/emp_exam/:id/:id2", async (req, res) => {
+  const train=req.params.id;
+  const sub=req.params.id2;
+  try {
+    pool.query('SELECT * FROM  emp_exam_result where emp_training=(?) and emp_subject=(?)',[train,sub], (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json(0);
+      } else {
+        const heading = [["Employee_id", "Training", "Subject", "Marks", "Out_Of"]]
+        const workbook = XLSX.utils.book_new()
+        const worksheet = XLSX.utils.json_to_sheet(result)
+        XLSX.utils.sheet_add_aoa(worksheet, heading)
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'employee')
+        const buffer = XLSX.write(workbook, { booktype: 'xlsx', type: 'buffer' })
+        res.attachment("result.xlsx")
+        return res.send(buffer)
+      }
+    });
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 //result showing
 
@@ -2050,33 +2059,34 @@ app.get("/attendence/absent/:id/:name/:utc/:subject/:training", (req, res) => {
 });
 
 
-app.get('/attendence/excel/:training/:subject', (req, res) => {
-  let train=req.params.training
-  let sub=req.params.subject
-  pool.query('SELECT emp_id , emp_name , emp_training,emp_subject,emp_date,pa  FROM  attendence where emp_training=(?) and emp_subject=(?) ',[train,sub], (error, results) => {
-    if (error) throw error;
-    var data = [];
-    var headers = ["Employee_id", "Name", "Training", "Subject", "Date", "Attendence Status"];
-    data.push(headers);
-    results.forEach(row => {
-      data.push([row["emp_id"],row["emp_name"], row["emp_training"], row["emp_subject"], row["emp_date"], row["pa"]]);
+app.get('/attendence/excel/:id/:id2/:id3', (req, res) => {
+  const train=req.params.id;
+  const sub=req.params.id2;
+  const date=req.params.id3;
+  console.log(train+sub+date)
+  try {
+    pool.query('SELECT * FROM attendence where emp_training=(?) and emp_subject=(?) and emp_date=(?)',[train,sub,date], (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(200).json("same date is already");
+      } else {
+        
+        const heading = [["Employee_id", "Name" ,"Training", "Subject", "Date", "P/A"]]
+        const workbook = XLSX.utils.book_new()
+        const worksheet = XLSX.utils.json_to_sheet(result)
+        XLSX.utils.sheet_add_aoa(worksheet, heading)
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'employee')
+        const buffer = XLSX.write(workbook, { booktype: 'xlsx', type: 'buffer' })
+        res.attachment("result.xlsx")
+        return res.send(buffer)
+      }
     });
-    console.log(data,"in excel")
-
-    // (C2) WRITE TO EXCEL FILE
-    var worksheet = XLSX.utils.aoa_to_sheet(data),
-      workbook = XLSX.utils.book_new();
-    const downloadFolder = 'downloads';
-    const fileName = 'attendence_emp.xlsx';
-    const downloadPath = path.join(__dirname, fileName);
-
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Users");
-
-    XLSX.writeFile(workbook, downloadPath);
-    res.json(1);
+  } catch (error) {
+    console.log(error)
+  }
 
   });
-});
+
 
 
 const PORT = 3000;
